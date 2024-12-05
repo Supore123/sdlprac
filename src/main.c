6fc4dafb565542ec90915aeeb5c461b477ab3f04
Supore123 <jonathanyohannes421@gmail.com>
2024-12-05 23:46:49 +0000

    init commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/* Includes */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "app.h"
+
+
+/* Included functions */
+extern void userInit(void);
+
+//
+//	main() - Main Start Function
+//
+int main()
+{
+	userInit();
+	return 0;
+}
