71e4d883ada5ef461a9d64f1c314d51d588aeeaa
Supore123 <jonathanyohannes421@gmail.com>
2024-12-06 13:16:22 +0000

    Reconfigured appConfig states and appStatus for devices

--- include/app.h
+++ include/app.h
@@ -1,13 +1,12 @@
 // Includes
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 // Hard defines
 #define APP_VERSION 		0x01
 #define APP_VERSION_STR		"0.01"
-
-// Function defines
-void appInit(void);
 
 // Variable defines
 typedef enum appStatus
@@ -17,3 +16,14 @@ typedef enum appStatus
 	APP_STATUS_UNKNOWN,
 	APP_STATUS_RESERVED,
 }appStatus_t;
+
+typedef struct appConfig
+{
+	uint32_t version;
+}appConfig_t;
+
+// Function defines
+void appInit(void);
+appStatus_t NvmDataWrite(void);
+
+
--- src/app.c
+++ src/app.c
@@ -2,16 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
-typedef struct appConfig
-{
-	uint32_t version;
-}appConfig_t;
+#include <stdbool.h>
 
 appConfig_t appConfig = {0};
 
+// userInit -- Initialises all parameters
 void userInit(void)
 {
-	appConfig.version = APP_VERSION;
+	FILE *appFile;
+	bool loadDefaults = false;
+	appFile = fopen("appFiles.txt", "w+");
+	
+	if(appFile == 0)
+	{
+		loadDefaults = true;
+	}
+
+	if(loadDefaults == true)
+	{
+		printf("Loading Factory Defaults");
+		appConfig.version = APP_VERSION; // Make the defaults here, then make a function call to save
+	}
+
 	printf("Loading App Version 0x0%u\r\n", appConfig.version);
+}
+
+appStatus_t NvmWriteData(void)
+{
+	appStatus_t sc = APP_STATUS_OK;
+
+	printf("Writing Data...\r\n");
+
+	return sc;
 }
