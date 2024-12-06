eab9d83f8a5c42605dbd87c4b212666ff2add60a
Supore123 <jonathanyohannes421@gmail.com>
2024-12-06 00:14:50 +0000

    Setup Status scenario for app version Setup app version for changing versions

--- include/app.h
+++ include/app.h
@@ -1,4 +1,19 @@
+// Includes
 #include <stdio.h>
 #include <stdlib.h>
 
+// Hard defines
+#define APP_VERSION 		0x01
+#define APP_VERSION_STR		"0.01"
+
+// Function defines
 void appInit(void);
+
+// Variable defines
+typedef enum appStatus
+{
+	APP_STATUS_OK,
+	APP_STATUS_FAIL,
+	APP_STATUS_UNKNOWN,
+	APP_STATUS_RESERVED,
+}appStatus_t;
--- src/app.c
+++ src/app.c
@@ -1,7 +1,17 @@
 #include "app.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct appConfig
+{
+	uint32_t version;
+}appConfig_t;
+
+appConfig_t appConfig = {0};
 
 void userInit(void)
 {
-	printf("Init Routine\r\n");
+	appConfig.version = APP_VERSION;
+	printf("Loading App Version 0x0%u\r\n", appConfig.version);
 }
