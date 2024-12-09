4371162bedf9cf3b7372decb970d236a55a9806a
Supore123 <jonathanyohannes421@gmail.com>
2024-12-09 01:03:14 +0000

    restored screens to prior settings

--- src/screen.c
+++ src/screen.c
@@ -1,53 +1,22 @@
+#include <time.h>
 #include "screen.h"
 #include "SDL2/SDL.h"
 
+// vars
+Window_t *mainWin = {NULL};
+
+// screenInit -- Starts the screen initilisation process
 appStatus_t screenInit(void)
 {
     appStatus_t sc = APP_STATUS_OK;
+    
+    return sc;
+}
 
-    SDL_Window *window = NULL; // initialise a window
+// loadImage -- Loads an image to a screen
+appStatus_t loadImage(void)
+{
+    appStatus_t sc = APP_STATUS_OK;
 
-    // The surface contained by the window
-    SDL_Surface *screenSurface = NULL;
-
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        sc = APP_STATUS_SCREEN_FAILURE;
-    }
-    else
-    {
-        // Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, MAIN_SCREEN_WIDTH, MAIN_SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            // Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
-
-            // Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-            // Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            // Hack to get window to stay up
-            SDL_Event e;
-            bool quit = false;
-            while (quit == false)
-            {
-                while (SDL_PollEvent(&e))
-                {
-                    if (e.type == SDL_QUIT)
-                        quit = true;
-                }
-            }
-        }
-    }
-
-        return sc;
+    return sc;
 }
